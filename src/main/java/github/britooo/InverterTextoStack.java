ac66d3012db4734a10cbbb14f877fe925b3f3510
Robert1730 <robert.souza@sptech.school>
2026-04-08 18:40:51 -0300

    Finalizado

--- src/main/java/github/britooo/InverterTextoStack.java
+++ src/main/java/github/britooo/InverterTextoStack.java
@@ -8,6 +8,24 @@ public class InverterTextoStack {
 
             Então, não fique pilhado, use a classe Pilha.
         */
-        return null;
+
+        Integer tamanho = input.length();
+
+        Pilha<String> pilha = new Pilha<String>(tamanho);
+
+        for (int i = 0; i < tamanho; i++) {
+            System.out.println(String.valueOf(input.charAt(i)));
+            pilha.push(String.valueOf(input.charAt(i)));
+        }
+
+        System.out.println(pilha.toString());
+
+        String textoInvertido = "";
+
+        while (!pilha.isEmpty()) {
+            textoInvertido += pilha.pop();
+        }
+
+        return textoInvertido;
     }
 }
